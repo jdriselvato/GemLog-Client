3a7fb63caa592d0f4794d34c5afeb2dd81ad456f
jdriselvato <jdriselvato@gmail.com>
2023-02-24 16:39:35 -0500

    basic layout complete. needs functionality

--- source/client.py
+++ source/client.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 import PySimpleGUI as sg
+import webbrowser
 
 # Theme
 
-sg.theme('DarkAmber')
+# sg.theme('DarkAmber')
 
 # Constants
 
-# Window init (Bebo's old header size)
-
-window_width = 760
-window_height = 375
-
 layout = [
-    [sg.Canvas(size=(window_width, window_height), key='canvas')],
     [
-        sg.Button('Post'), # hint button
-        sg.Text('', key='display_note_text'), # hint / tip text
-        sg.Push(),
-        sg.Text('', key='display_score', text_color="#2FF599"), # score text
+        sg.Text('title:', size =(15, 1), key='username'), sg.InputText(),
     ],
+    [
+        sg.Text('Post content* (text/gemini format):'), # hint / tip text
+    ],
+    [
+        sg.Multiline(size=(120, 40), key='textbox'),
+    ],
+    [
+        sg.Text('Username:', size =(9, 1), key='username'), sg.InputText(),
+        sg.Text('Password:', size =(9, 1), key='password'), sg.InputText(),
+        sg.Button('Post'), # hint button
+    ],
+    [
+        sg.Text("Read more here: https://gemlog.blue", tooltip="https://gemlog.blue", enable_events=True, key=f'URL {"https://gemlog.blue"}')
+    ]
 ]
 
 # Create the Window
 
-window = sg.Window('GemLog.blue Client', layout, return_keyboard_events=True, finalize=True)
+window = sg.Window('GemLog.blue Client - Add an entry to your gemlog', layout, return_keyboard_events=True, finalize=True)
 window.maximize()
 
 
@@ -35,5 +41,12 @@ while True:
     print(event)
     if event == sg.WIN_CLOSED:
         break
+    elif event.startswith("URL "):
+            url = event.split(' ')[1]
+            webbrowser.open(url)
     elif event == 'Post':
-        print(event)
+        postTitle = values['title']
+        postContent = values['textbox']
+        password = values['username']
+        password = values['password']
+        print(event, postContent, username, password)
